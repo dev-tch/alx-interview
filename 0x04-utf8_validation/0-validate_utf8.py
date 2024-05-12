02b32e992aeb1432c5945f3f6dbf54cf692c451f
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-12 15:23:29 +0000

    fix validation utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,23 @@
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding """
-    lent_byte = 0
+    bytes_remaining = 0
     for item in data:
         if not isinstance(item, int):
             return False
-        if lent_byte == 0:
-            if item >> 5 == 0b110:
-                lent_byte = 1
-            elif item >> 4 == 0b1110:
-                lent_byte = 2
-            elif item >> 3 == 0b11110:
-                lent_byte = 3
-            elif item >> 7 != 0:
+        if bytes_remaining == 0:
+            if item & 0b10000000 == 0:
+                bytes_remaining = 0
+            elif item & 0b11100000 == 0b11000000:
+                bytes_remaining = 1
+            elif item & 0b11110000 == 0b11100000:
+                bytes_remaining = 2
+            elif item & 0b11111000 == 0b11110000:
+                bytes_remaining = 3
+            else:
                 return False
         else:
-            if item >> 6 != 0b10:
+            if item & 0b11000000 != 0b10000000:
                 return False
-            lent_byte -= 1
-    return (lent_byte == 0)
+            bytes_remaining -= 1
+    return bytes_remaining == 0
