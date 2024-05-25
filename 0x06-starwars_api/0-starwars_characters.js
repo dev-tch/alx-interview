24c75ba9548f1e0155911ae098432aa61ca92437
dev-tch <tijeni.chaabeni@gmail.com>
2024-05-25 02:04:05 +0000

    work with promise and await to handle asynchronous tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,28 @@
 const request = require('request');
 if (process.argv.length === 3) {
   const url = 'https://swapi-api.alx-tools.com/api' + '/films/' + process.argv[2];
-  request({ url }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      JSON.parse(body).characters.forEach((api) => {
-        request(api, function (error, response, body) {
-          if (!error && response.statusCode === 200) {
-            console.log(JSON.parse(body).name);
-          }
-        });
-      }
-      );
+  const getBodyJson = (url) => {
+    return new Promise((resolve, reject) => {
+      request({ url }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          resolve(JSON.parse(body));
+        } else {
+          reject(error || new Error(`Status code: ${response.statusCode}`));
+        }
+      });
+    });
+  };
+  const namesCharacters = async () => {
+    try {
+      const MoviePromise = await getBodyJson(url);
+      const ListApiUrlPromise = MoviePromise.characters.map(getBodyJson);
+      const CharactersPromise = await Promise.all(ListApiUrlPromise);
+      CharactersPromise.forEach((character) => {
+        console.log(character.name);
+      });
+    } catch (error) {
+      console.error(error);
     }
-  });
+  };
+  namesCharacters();
 }
