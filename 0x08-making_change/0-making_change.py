598cdc8a1c1b5ef5f4792b557b07552b2c2cc2c3
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-05 23:34:03 +0000

    backtracking coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" module with one function """
+
+
+def search_coins(coins, idx_coin, sum_coins, nb_coins, total, stoarge):
+    """ this method used for recursiive calls and backtracking"""
+    if sum_coins == total:
+        stoarge['nb_coins'] = nb_coins
+        return True
+    if sum_coins > total or idx_coin == len(coins):
+        return False
+    # choose index
+    if search_coins(coins, idx_coin, sum_coins + coins[idx_coin],
+                    nb_coins + 1, total, stoarge):
+        return True
+    # back tack and attempt onother index
+    if search_coins(coins, idx_coin + 1, sum_coins, nb_coins,
+                    total, stoarge):
+        return True
+    return False
+
+
+def makeChange(coins, total):
+    """ find the few number of coins that it's sum equals value total"""
+    if total == 0:
+        return 0
+    stoarge = {'nb_coins': None}
+    # sort descending
+    coins.sort(reverse=True)
+    search_coins(coins, 0, 0, 0, total, stoarge)
+    solution = stoarge.get('nb_coins')
+    if solution is None:
+        return -1
+    return solution
