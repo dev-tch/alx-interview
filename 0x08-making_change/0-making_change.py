da5e12d66f085cfd586f1cd47b0f3d96afdd5540
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-06 13:01:21 +0000

    fix coins01

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ module with one function """
-import math
 
 
 def makeChange(coins, total):
@@ -9,14 +8,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     # choose value  biggest that total it can be INFINITY
-    # let say math.inf > total
+    # or any random big NUMBER > total
     # initialize array of size(total + 1) with choosed MAX value
     # init index 0 with 0 : cause with amount 0 we have 0 coins
     size = (total + 1)
-    U = [math.inf] * size
+    MAXNUMBER = total + 1
+    U = [MAXNUMBER] * size
     U[0] = 0
 
     for n in range(1, size):
-        U[n] = min(U[n - c] + 1 if n - c >= 0 else math.inf
+        U[n] = min(U[n - c] + 1 if n - c >= 0 else MAXNUMBER
                    for c in coins)
-    return U[total] if U[total] != math.inf else -1
+    return U[total] if U[total] != MAXNUMBER else -1
